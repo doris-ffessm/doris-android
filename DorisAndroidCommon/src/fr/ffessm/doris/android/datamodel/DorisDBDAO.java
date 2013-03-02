53e75ad05d0f28eac7c3a376edca7882b9f6316b
Didier Vojtisek <dvojtise@gmail.com>
2013-03-02 14:15:51 +0100

    Use header in the generated files for DataModel

--- DorisAndroidCommon/src/fr/ffessm/doris/android/datamodel/DorisDBDAO.java
+++
@@ -1,8 +0,0 @@
-package fr.ffessm.doris.android.datamodel;
-
-import com.j256.ormlite.dao.RuntimeExceptionDao;
-
-public class DorisDBDAO {
-	protected RuntimeExceptionDao<Participant, Integer> participantDao;
-
-}
--- DorisAndroidCommon/src/fr/ffessm/doris/android/datamodel/xml/ZoneObservationXMLParser.java
+++ DorisAndroidCommon/src/fr/ffessm/doris/android/datamodel/xml/ZoneObservationXMLParser.java
@@ -1,14 +1,45 @@
+/* *********************************************************************
+ * Licence CeCILL-B
+ * *********************************************************************
+Copyright du Code : A COMPLÉTER  ([16/02/2013]) 
 
-/*******************************************************************************
- * Copyright (c) 2012 Vojtisek.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- * 
- * Contributors:
- *     Didier Vojtisek - initial API and implementation
- *******************************************************************************/
+A COMPLÉTER : A COMPLÉTER@gmail.com
+
+Ce logiciel est un programme informatique servant à afficher de manière 
+ergonomique sur un terminal Android les fiches du site : doris.ffessm.fr. 
+
+Les images, logos et textes restent la propriété de leurs auteurs, cf. : 
+doris.ffessm.fr.
+
+Ce logiciel est régi par la licence CeCILL-B soumise au droit français et
+respectant les principes de diffusion des logiciels libres. Vous pouvez
+utiliser, modifier et/ou redistribuer ce programme sous les conditions
+de la licence CeCILL-B telle que diffusée par le CEA, le CNRS et l'INRIA 
+sur le site "http://www.cecill.info".
+
+En contrepartie de l'accessibilité au code source et des droits de copie,
+de modification et de redistribution accordés par cette licence, il n'est
+offert aux utilisateurs qu'une garantie limitée.  Pour les mêmes raisons,
+seule une responsabilité restreinte pèse sur l'auteur du programme,  le
+titulaire des droits patrimoniaux et les concédants successifs.
+
+A cet égard  l'attention de l'utilisateur est attirée sur les risques
+associés au chargement,  à l'utilisation,  à la modification et/ou au
+développement et à la reproduction du logiciel par l'utilisateur étant 
+donné sa spécificité de logiciel libre, qui peut le rendre complexe à 
+manipuler et qui le réserve donc à des développeurs et des professionnels
+avertis possédant  des  connaissances  informatiques approfondies.  Les
+utilisateurs sont donc invités à charger  et  tester  l'adéquation  du
+logiciel à leurs besoins dans des conditions permettant d'assurer la
+sécurité de leurs systèmes et ou de leurs données et, plus généralement, 
+à l'utiliser et l'exploiter dans les mêmes conditions de sécurité. 
+
+Le fait que vous puissiez accéder à cet en-tête signifie que vous avez 
+pris connaissance de la licence CeCILL-B, et que vous en avez accepté les
+termes.
+* Auteurs : Guillaume Mo <gmo7942@gmail.com>
+*           Didier Vojtisek <dvojtise@gmail.com>
+* ********************************************************************* */
 package fr.ffessm.doris.android.datamodel.xml;
 
 import org.xml.sax.helpers.DefaultHandler;
