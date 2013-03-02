998621da6f285829255c388e814aa9bcfdd4cf04
Didier Vojtisek <dvojtise@gmail.com>
2013-03-02 21:04:05 +0100

    - added some of the planned view and background activities - draft of yuml activity diagram

---
+++ DorisAndroid/src/fr/ffessm/doris/android/activities/DetailsFiche_ElementViewActivity.java
@@ -0,0 +1,105 @@
+/*******************************************************************************
+ * Copyright (c) 2012 Vojtisek.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Didier Vojtisek - initial API and implementation
+ *******************************************************************************/
+package fr.ffessm.doris.android.activities;
+
+
+import fr.ffessm.doris.android.datamodel.Fiche;
+import fr.ffessm.doris.android.datamodel.OrmLiteDBHelper;
+import fr.ffessm.doris.android.R;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.j256.ormlite.android.apptools.OrmLiteBaseActivity;
+import com.j256.ormlite.dao.RuntimeExceptionDao;
+
+
+public class DetailsFiche_ElementViewActivity extends OrmLiteBaseActivity<OrmLiteDBHelper>{
+	
+	protected int ficheId;
+	
+	/** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.detailsfiche_elementview);
+        ficheId = getIntent().getExtras().getInt("ficheId");
+        //findViewById(R.id.divedate);
+    }
+    
+    @Override
+	protected void onResume() {
+		super.onResume();
+		refreshScreenData();
+	}
+    
+    
+    private void refreshScreenData() {
+    	// get our dao
+    	RuntimeExceptionDao<Fiche, Integer> entriesDao = getHelper().getFicheDao();
+    	Fiche entry = entriesDao.queryForId(ficheId);
+    	
+		((TextView) findViewById(R.id.detailsfiche_elementview_nomscientifique)).setText(entry.getNomScientifique());
+		((TextView) findViewById(R.id.detailsfiche_elementview_nomcommun)).setText(entry.getNomCommun());
+		((TextView) findViewById(R.id.detailsfiche_elementview_numerofiche)).setText(((Integer)entry.getNumeroFiche()).toString());					
+		((TextView) findViewById(R.id.detailsfiche_elementview_autresdenominations)).setText(entry.getAutresDenominations());
+    	/*SimpleDateFormat dateFormatter = new SimpleDateFormat("MM/dd/yyyy HH:mm");
+    	((TextView) findViewById(R.id.detail_divedate)).setText(dateFormatter.format(entry.getDate()));
+		
+    	((TextView) findViewById(R.id.detail_divelocation)).setText(entry.getLocation());
+    	
+    	((TextView) findViewById(R.id.detail_divedepth)).setText(entry.getMaxdepth().toString());
+    	
+    	((TextView) findViewById(R.id.detail_diveduration)).setText(entry.getDuration().toString());
+    	*/
+	}
+
+	@Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+		// add options in the menu
+        menu.add(Menu.NONE, 777, 0, R.string.preference_menu_title).setIcon(android.R.drawable.ic_menu_preferences);
+        
+		//Start of user code additional onCreateOptionsMenu
+
+		//End of user code
+        return super.onCreateOptionsMenu(menu);
+    }
+    
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+    	// behavior of option menu
+        switch (item.getItemId()) {
+        	case 777:
+                    startActivity(new Intent(this, Preference_PreferenceViewActivity.class));
+                    return true;
+        
+		//Start of user code additional menu action
+
+		//End of user code
+        }
+        return false;
+    }
+
+	private void showToast(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+    }
+}
---
+++ DorisAndroid/src/fr/ffessm/doris/android/activities/ListeFicheAvecFiltreAdapter.java
@@ -0,0 +1,95 @@
+/*******************************************************************************
+ * Copyright (c) 2012 Vojtisek.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Didier Vojtisek - initial API and implementation
+ *******************************************************************************/
+package fr.ffessm.doris.android.activities;
+
+
+import java.util.List;
+
+import fr.ffessm.doris.android.R;
+import fr.ffessm.doris.android.datamodel.Fiche;
+
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.j256.ormlite.dao.RuntimeExceptionDao;
+
+public class ListeFicheAvecFiltreAdapter extends BaseAdapter{
+	
+private Context context;
+
+    private List<Fiche> ficheList;
+
+	public ListeFicheAvecFiltreAdapter(Context context, RuntimeExceptionDao<Fiche, Integer> entriesDao) {
+		super();
+		this.context = context;
+		// TODO find a way to query in a lazy way
+		this.ficheList = entriesDao.queryForAll();
+	}
+
+	@Override
+	public int getCount() {
+		return ficheList.size();
+	}
+
+	@Override
+	public Object getItem(int position) {
+		return ficheList.get(position);
+
+	}
+
+	@Override
+	public long getItemId(int position) {
+		return position;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup viewGroup) {
+		Fiche entry = ficheList.get(position);
+        if (convertView == null) {
+            LayoutInflater inflater = (LayoutInflater) context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.listeficheavecfiltre_listviewrow, null);
+        }
+       
+		// set data in the row 
+		TextView tvLabel = (TextView) convertView.findViewById(R.id.listeficheavecfiltre_listviewrow_label);
+        StringBuilder labelSB = new StringBuilder();
+				labelSB.append(entry.getImageVignette().toString());
+			
+			labelSB.append(" ");
+        tvLabel.setText(labelSB.toString());
+
+        TextView tvDetails = (TextView) convertView.findViewById(R.id.listeficheavecfiltre_listviewrow_details);
+		StringBuilder detailsSB = new StringBuilder();
+		detailsSB.append(entry.getNom().toString());
+		detailsSB.append(" ");
+        tvDetails.setText(detailsSB.toString());
+		
+        // assign the entry to the row in order to ease GUI interactions
+        LinearLayout llRow = (LinearLayout)convertView.findViewById(R.id.listeficheavecfiltre_listviewrow);
+        llRow.setTag(entry);
+        
+        return convertView;
+
+	}
+
+	
+}
