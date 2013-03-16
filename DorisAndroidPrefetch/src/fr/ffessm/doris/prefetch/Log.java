01ffb76017099e7073fb82cf08263b861ef938e4
Guillaume Mo <gmo7942@gmail.com>
2013-03-16 23:49:53 +0100

    Mise en place des arguments de l'application

---
+++ DorisAndroidPrefetch/src/fr/ffessm/doris/prefetch/Log.java
@@ -0,0 +1,91 @@
+
+/* *********************************************************************
+ * Licence CeCILL-B
+ * *********************************************************************
+Copyright du Code : A COMPLÉTER  ([16/02/2013]) 
+
+A COMPLÉTER : A COMPLÉTER@gmail.com
+
+Ce logiciel est un programme informatique servant à afficher de manière 
+ergonomique sur un terminal Android les fiches du site : doris.ffessm.fr. 
+
+Les images, logos et textes restent la propriété de leurs auteurs, cf. : 
+doris.ffessm.fr.
+
+Ce logiciel est régi par la licence CeCILL-B soumise au droit français et
+respectant les principes de diffusion des logiciels libres. Vous pouvez
+utiliser, modifier et/ou redistribuer ce programme sous les conditions
+de la licence CeCILL-B telle que diffusée par le CEA, le CNRS et l'INRIA 
+sur le site "http://www.cecill.info".
+
+En contrepartie de l'accessibilité au code source et des droits de copie,
+de modification et de redistribution accordés par cette licence, il n'est
+offert aux utilisateurs qu'une garantie limitée.  Pour les mêmes raisons,
+seule une responsabilité restreinte pèse sur l'auteur du programme,  le
+titulaire des droits patrimoniaux et les concédants successifs.
+
+A cet égard  l'attention de l'utilisateur est attirée sur les risques
+associés au chargement,  à l'utilisation,  à la modification et/ou au
+développement et à la reproduction du logiciel par l'utilisateur étant 
+donné sa spécificité de logiciel libre, qui peut le rendre complexe à 
+manipuler et qui le réserve donc à des développeurs et des professionnels
+avertis possédant  des  connaissances  informatiques approfondies.  Les
+utilisateurs sont donc invités à charger  et  tester  l'adéquation  du
+logiciel à leurs besoins dans des conditions permettant d'assurer la
+sécurité de leurs systèmes et ou de leurs données et, plus généralement, 
+à l'utiliser et l'exploiter dans les mêmes conditions de sécurité. 
+
+Le fait que vous puissiez accéder à cet en-tête signifie que vous avez 
+pris connaissance de la licence CeCILL-B, et que vous en avez accepté les
+termes.
+* ********************************************************************
+* ********************************************************************* */
+
+package fr.ffessm.doris.prefetch;
+
+/* *********************************************************************
+ * Log
+ ********************************************************************** */
+public class Log {
+    private final static String LOGTAG = "Log";
+
+    public final int LOG_SILENCE = 9;
+    public final int LOG_MESSAGE = 4;
+    public final int LOG_WARNING = 3;
+    public final int LOG_ERROR = 2;
+    public final int LOG_VERBOSE = 1;
+    public final int LOG_DEBUG = 0;
+    
+    public static int niveauTrace;
+    
+    Log (){
+    	niveauTrace = LOG_WARNING;
+    	
+    	log(LOG_DEBUG, LOGTAG, "Log() - Début");
+
+    	log(LOG_DEBUG, LOGTAG, "Log() - Fin");
+    }
+    
+    Log (int niveauTraceIn){
+    	niveauTrace = niveauTraceIn;
+    }
+    
+    public void set_niveauTrace (int niveauTraceIn){
+    	log(LOG_DEBUG, LOGTAG, "set_niveauTrace() - Début");
+    	log(LOG_DEBUG, LOGTAG, "set_niveauTrace() - niveauTraceIn : " + niveauTraceIn);
+    	niveauTrace = niveauTraceIn;
+    	
+    	log(LOG_DEBUG, LOGTAG, "set_niveauTrace() - Fin");
+    } 
+	
+    public void log(int typeTrace, String tag, String string) {
+
+		if (typeTrace >= niveauTrace) {
+			String texte = tag + " - " + string;
+			System.out.println(texte);
+		}
+		
+	}
+
+
+}
--- DorisAndroidPrefetch/src/fr/ffessm/doris/prefetch/PrefetchDorisWebSite.java
+++ DorisAndroidPrefetch/src/fr/ffessm/doris/prefetch/PrefetchDorisWebSite.java
@@ -1,3 +1,45 @@
+/* *********************************************************************
+ * Licence CeCILL-B
+ * *********************************************************************
+ * Copyright (c) 2012-2013 - FFESSM
+ * Auteurs : Guillaume Mo <gmo7942@gmail.com>
+ *           Didier Vojtisek <dvojtise@gmail.com>
+ * *********************************************************************
+
+Ce logiciel est un programme informatique servant à afficher de manière 
+ergonomique sur un terminal Android les fiches du site : doris.ffessm.fr. 
+
+Les images, logos et textes restent la propriété de leurs auteurs, cf. : 
+doris.ffessm.fr.
+
+Ce logiciel est régi par la licence CeCILL-B soumise au droit français et
+respectant les principes de diffusion des logiciels libres. Vous pouvez
+utiliser, modifier et/ou redistribuer ce programme sous les conditions
+de la licence CeCILL-B telle que diffusée par le CEA, le CNRS et l'INRIA 
+sur le site "http://www.cecill.info".
+
+En contrepartie de l'accessibilité au code source et des droits de copie,
+de modification et de redistribution accordés par cette licence, il n'est
+offert aux utilisateurs qu'une garantie limitée.  Pour les mêmes raisons,
+seule une responsabilité restreinte pèse sur l'auteur du programme,  le
+titulaire des droits patrimoniaux et les concédants successifs.
+
+A cet égard  l'attention de l'utilisateur est attirée sur les risques
+associés au chargement,  à l'utilisation,  à la modification et/ou au
+développement et à la reproduction du logiciel par l'utilisateur étant 
+donné sa spécificité de logiciel libre, qui peut le rendre complexe à 
+manipuler et qui le réserve donc à des développeurs et des professionnels
+avertis possédant  des  connaissances  informatiques approfondies.  Les
+utilisateurs sont donc invités à charger  et  tester  l'adéquation  du
+logiciel à leurs besoins dans des conditions permettant d'assurer la
+sécurité de leurs systèmes et ou de leurs données et, plus généralement, 
+à l'utiliser et l'exploiter dans les mêmes conditions de sécurité. 
+
+Le fait que vous puissiez accéder à cet en-tête signifie que vous avez 
+pris connaissance de la licence CeCILL-B, et que vous en avez accepté les
+termes.
+* ********************************************************************* */
+
 package fr.ffessm.doris.prefetch;
 
 import java.io.BufferedWriter;
@@ -19,6 +61,7 @@ import com.j256.ormlite.stmt.SelectArg;
 import com.j256.ormlite.support.ConnectionSource;
 import com.j256.ormlite.table.TableUtils;
 
+
 import fr.ffessm.doris.android.datamodel.DorisDBHelper;
 import fr.ffessm.doris.android.datamodel.Fiche;
 import fr.ffessm.doris.android.datamodel.Participant;
@@ -32,17 +75,37 @@ import fr.ffessm.doris.android.datamodel.associations.Fiches_verificateurs_Parti
 
 public class PrefetchDorisWebSite {
 
+	// Pourrait être un jour utile, on verra
+	private final static String VERSION = "0.01";
+	
 	// we are using the in-memory H2 database
 	private final static String DATABASE_URL = "jdbc:h2:mem:fiche";
 
+	// Inititalisation de la Gestion des Log {Log(0) permet de forcer le mode debbug}
+	private final static String LOGTAG = "PrefetchDoris";
+	public static Log trace = new Log();
+	//public static Log trace = new Log(0);
+	
+	//Nombre maximum de fiches traitées (--max=K permet de changer cette valeur)
+	private static int nbMaxFichesTraitees = 9999;
+	
 	DorisDBHelper dbContext = null;
 
 	public static void main(String[] args) throws Exception {
+		
 		// turn our static method into an instance of Main
 		new PrefetchDorisWebSite().doMain(args);
+		
 	}
 
 	private void doMain(String[] args) throws Exception {
+		
+		//Vérification et lecture des arguments
+		trace.log(trace.LOG_DEBUG, LOGTAG, "doMain() : Vérification et lecture des arguments");
+		String action = checkArgs(args);
+		trace.log(trace.LOG_VERBOSE, LOGTAG, "action : " + action);
+		trace.log(trace.LOG_VERBOSE, LOGTAG, "Nb. Fiches Max : " + nbMaxFichesTraitees);
+		
 		ConnectionSource connectionSource = null;
 		try {
 			// create our data-source for the database
@@ -60,6 +123,104 @@ public class PrefetchDorisWebSite {
 		}
 	}
 
+	/**
+	 * Vérification des arguments passés à l'application
+	 * 
+	 *  @param args
+	 */
+	private String checkArgs(String[] args){
+			
+		// Si Aucun Argument, on affiche l'aide et on termine
+		trace.log(trace.LOG_DEBUG, LOGTAG, "checkArgs() - nb args : " + args.length);
+		if (args.length < 1) {
+			help();
+			trace.log(trace.LOG_ERROR, LOGTAG, "Le programme ne peut être lancé sans arguments.");
+			System.exit(0);
+		}
+		
+		// On commence par regarder si un des paramètres est un paramétre optionnel prioritaire
+		// verbose, debug ou silence
+		trace.log(trace.LOG_DEBUG, LOGTAG, "checkArgs() - debug, verbose ou silence ? ");
+		for (String arg : args) {
+			trace.log(trace.LOG_DEBUG, LOGTAG, "checkArgs() - arg : " + arg);
+			
+			if ( arg.equals("-d") || arg.equals("--debug") ) {
+				trace.set_niveauTrace(trace.LOG_DEBUG);
+			}
+			if ( arg.equals("-v") || arg.equals("--verbose")) {
+				trace.set_niveauTrace(trace.LOG_VERBOSE);
+			}
+			if ( arg.equals("-s") || arg.equals("--silence")) {
+				trace.set_niveauTrace(trace.LOG_SILENCE);
+			}
+		}
+		
+		// Si Aide ou Version alors affichage puis on termine
+		trace.log(trace.LOG_DEBUG, LOGTAG, "checkArgs() - help ou version ? ");
+		for (String arg : args) {
+			
+			if ( arg.equals("-h") || arg.equals("--help")) {
+				trace.log(trace.LOG_DEBUG, LOGTAG, "checkArgs() - arg : " + arg);
+				help();
+				System.exit(0);
+			}
+			if ( arg.equals("-V") || arg.equals("--version")) {
+				trace.log(trace.LOG_DEBUG, LOGTAG, "checkArgs() - arg : " + arg);
+				version();
+				System.exit(0);
+			}
+		}
+		
+		// paramètre qui permet de limiter le nombre de fiches à traiter
+		trace.log(trace.LOG_DEBUG, LOGTAG, "checkArgs() - max ? ");
+		for (String arg : args) {
+			if ( arg.startsWith("-M") || arg.startsWith("--max=")) {
+				trace.log(trace.LOG_DEBUG, LOGTAG, "checkArgs() - arg : " + arg);
+				String nbFichesStr = null;
+				if ( arg.startsWith("-M")) {
+					nbFichesStr =  arg.substring(3);
+				}
+				if ( arg.startsWith("--max=")) {
+					nbFichesStr =  arg.substring(6);
+				}
+				try { 
+					 nbMaxFichesTraitees = Integer.parseInt(nbFichesStr);
+					 trace.log(trace.LOG_DEBUG, LOGTAG, "checkArgs() - nbMaxFichesTraitees : " + nbMaxFichesTraitees);
+			    } catch(NumberFormatException e) { 
+			    	help();
+					trace.log(trace.LOG_ERROR, LOGTAG, "Argument -M ou --max mal utilisé : " + arg);
+			    	System.exit(0);
+			    }
+			}
+		}
+
+			
+			
+		// Vérification que le dernier argument est une des actions prévues
+		String action = args[args.length - 1];
+		trace.log(trace.LOG_DEBUG, LOGTAG, "checkArgs() - argument action : " + action);
+
+		if (action.equals("INIT")) {
+			
+		} else if (action.equals("NEWFICHES")) {
+			
+		} else if (action.equals("UPDATE")) {
+			
+		} else if (action.equals("INITSSIMG")) {
+			
+		} else {
+			help();
+			String listeArgs = "";
+			for (String arg : args) {
+				listeArgs += arg + " ";
+			}
+			trace.log(trace.LOG_ERROR, LOGTAG, "arguments : " + listeArgs);
+			trace.log(trace.LOG_ERROR, LOGTAG, "Action non prévue");
+			System.exit(0);
+		}
+		return action;
+	}
+	
 	/**
 	 * Setup our database and DAOs
 	 */
@@ -80,7 +241,6 @@ public class PrefetchDorisWebSite {
 		// if you need to create the table
 		TableUtils.createTable(connectionSource, Fiche.class);
 		TableUtils.createTable(connectionSource, Participant.class);
-
 		TableUtils.createTable(connectionSource, PhotoFiche.class);
 		TableUtils.createTable(connectionSource, ZoneGeographique.class);
 		TableUtils.createTable(connectionSource, ZoneObservation.class);
@@ -98,7 +258,6 @@ public class PrefetchDorisWebSite {
 
 		Fiche fiche1 = new Fiche("Amphiprion bicinctus",
 				"Poisson-clown à deux bandes", 1001, "");
-		
 		Fiche fiche2 = new Fiche("Palaemon elegans Rathke",
 				"Petite crevette rose", 337, "");
 		// persist the fiches object to the database
@@ -107,7 +266,7 @@ public class PrefetchDorisWebSite {
 		
 		SectionFiche s1 = new SectionFiche("Titre S1","hqdshgh ojfjdj");
 		//fiche1.getContenu().add(s1);
-		s1.setFiche(fiche1);
+		//s1.setFiche(fiche1);
 		dbContext.sectionFicheDao.create(s1);
 		
 		int id = fiche1.getId();
@@ -158,6 +317,7 @@ public class PrefetchDorisWebSite {
 			out.write("<Fiches>\n");
 			for (Fiche fiche : fiches) {
 				out.write(fiche.toXML("",dbContext)+"\n");
+				//out.write(fiche.toXML()+"\n");
 			}
 			out.write("</Fiches>\n");
 			out.write("</Doris>\n");
@@ -166,6 +326,43 @@ public class PrefetchDorisWebSite {
 		} catch (Exception e) {// Catch exception if any
 			System.err.println("Error: " + e.getMessage());
 		}
+	}
+	
+	/**
+     * Afficher l'aide de l'application
+     **/	
+	private static void help(){
+		trace.log(trace.LOG_DEBUG, LOGTAG, "help() - Début");
+		
+		System.out.println("Récupération de la base de fiches pour DorisAndroid, Version : " + VERSION);
+		System.out.println("Usage: java -jar PrefetchDorisWebSite.jar [OPTIONS] [ACTION]");
+		System.out.println("");
+		System.out.println("OPTIONS :");
+		System.out.println("  -M, --max=K      on limite le travail au K 1ères fiches (utile en dev.)");
+		System.out.println("  -V, --version     afficher la version de l'application et quitter");
+		System.out.println("  -h, --help        afficher cette aide");
+		System.out.println("  -d, --debug       messages detinés aux développeurs de cette application");
+		System.out.println("  -v, --verbose     messages permettant de suivre l'avancé des traitements");
+		System.out.println("  -s, --silence     aucune sortie, même pas les erreurs");
+		System.out.println("");
+		System.out.println("ACTION :");
+		System.out.println("  INIT               Toutes les fiches sont retéléchargées sur doris.ffessm.fr et retraitées pour créer la base (images comprises)");
+		System.out.println("  NEWFICHES          Ne télécharge que les nouvelles fiches");
+		System.out.println("  UPDATE             En plus des nouvelles fiches, on retélécharge les fiches qui ont changées de statut");
+		System.out.println("  INITSSIMG          Comme INIT sauf que l'on ne retélécharge pas une image déjà connue");
+		
+		trace.log(trace.LOG_DEBUG, LOGTAG, "help() - Fin");
+	}
+	
+	/**
+     * Afficher la version de l'application
+     * */	
+	private static void version(){
+		trace.log(trace.LOG_DEBUG, LOGTAG, "version() - Début");
+		
+		System.out.println("Version : " + VERSION);
+		
+		trace.log(trace.LOG_DEBUG, LOGTAG, "version() - Fin");
 	}
 
 }
