e0060739b7622ee35f7b040a82f2962969ba00f8
Guillaume Mo <gmo7942@gmail.com>
2014-04-24 22:27:08 +0200

    Correction CD après modif. url photo dans la base

--- DorisAndroidPrefetch/src/fr/ffessm/doris/prefetch/PrefetchFiches.java
+++ DorisAndroidPrefetch/src/fr/ffessm/doris/prefetch/PrefetchFiches.java
@@ -353,24 +353,24 @@ public class PrefetchFiches {
 
 								if ( !photoFiche.getCleURL().isEmpty() ) {
 									// Vignettes
-									if( ! prefetchTools.isFileExistingPath( fichierImageRefRacine+PrefetchConstants.SOUSDOSSIER_VIGNETTES+photoFiche.getCleURL().replace(" ", "_") ) ){
-										if (prefetchTools.getFichierFromUrl(Constants.VIGNETTE_BASE_URL+"/"+photoFiche.getCleURL().replace(" ", "%20"), fichierImageRacine+PrefetchConstants.SOUSDOSSIER_VIGNETTES+photoFiche.getCleURL().replace(" ", "_"))) {
+									if( ! prefetchTools.isFileExistingPath( fichierImageRefRacine+PrefetchConstants.SOUSDOSSIER_VIGNETTES+"/"+photoFiche.getCleURL().replace(" ", "_") ) ){
+										if (prefetchTools.getFichierFromUrl(Constants.VIGNETTE_BASE_URL+"/"+photoFiche.getCleURL().replace(" ", "%20"), fichierImageRacine+PrefetchConstants.SOUSDOSSIER_VIGNETTES+"/"+photoFiche.getCleURL().replace(" ", "_"))) {
 										} else {
 											log.error("Une erreur est survenue lors de la récupération de la liste des fiches");
 											//System.exit(1);
 										}
 									}
 									// Qualité Intermédiaire
-									if( ! prefetchTools.isFileExistingPath( fichierImageRefRacine+PrefetchConstants.SOUSDOSSIER_MED_RES+photoFiche.getCleURL().replace(" ", "_") ) ){
-										if (prefetchTools.getFichierFromUrl(Constants.MOYENNE_BASE_URL+"/"+photoFiche.getCleURL().replace(" ", "%20"), fichierImageRacine+PrefetchConstants.SOUSDOSSIER_MED_RES+photoFiche.getCleURL().replace(" ", "_"))) {
+									if( ! prefetchTools.isFileExistingPath( fichierImageRefRacine+PrefetchConstants.SOUSDOSSIER_MED_RES+"/"+photoFiche.getCleURL().replace(" ", "_") ) ){
+										if (prefetchTools.getFichierFromUrl(Constants.MOYENNE_BASE_URL+"/"+photoFiche.getCleURL().replace(" ", "%20"), fichierImageRacine+PrefetchConstants.SOUSDOSSIER_MED_RES+"/"+photoFiche.getCleURL().replace(" ", "_"))) {
 										} else {
 											log.error("Une erreur est survenue lors de la récupération de la liste des fiches");
 											//System.exit(1);
 										}
 									}
 									// Haute Qualité 
-									if( ! prefetchTools.isFileExistingPath( fichierImageRefRacine+PrefetchConstants.SOUSDOSSIER_HI_RES+photoFiche.getCleURL().replace(" ", "_") ) ){
-										if (prefetchTools.getFichierFromUrl(Constants.GRANDE_BASE_URL+"/"+photoFiche.getCleURL().replace(" ", "%20"), fichierImageRacine+PrefetchConstants.SOUSDOSSIER_HI_RES+photoFiche.getCleURL().replace(" ", "_"))) {
+									if( ! prefetchTools.isFileExistingPath( fichierImageRefRacine+PrefetchConstants.SOUSDOSSIER_HI_RES+"/"+photoFiche.getCleURL().replace(" ", "_") ) ){
+										if (prefetchTools.getFichierFromUrl(Constants.GRANDE_BASE_URL+"/"+photoFiche.getCleURL().replace(" ", "%20"), fichierImageRacine+PrefetchConstants.SOUSDOSSIER_HI_RES+"/"+photoFiche.getCleURL().replace(" ", "_"))) {
 										} else {
 											log.error("Une erreur est survenue lors de la récupération de la liste des fiches");
 											//System.exit(1);
