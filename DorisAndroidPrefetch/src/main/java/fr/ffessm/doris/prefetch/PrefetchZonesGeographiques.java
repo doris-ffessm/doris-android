b6ad3cf24400987670e644c7b10d483223b67bc5
Didier Vojtisek <didier.vojtisek@inria.fr>
2023-09-02 23:07:01 +0200

    guyanne is sub group of caraibe

--- DorisAndroidPrefetch/src/main/java/fr/ffessm/doris/prefetch/PrefetchZonesGeographiques.java
+++ DorisAndroidPrefetch/src/main/java/fr/ffessm/doris/prefetch/PrefetchZonesGeographiques.java
@@ -114,7 +114,7 @@ public class PrefetchZonesGeographiques {
                             new ZoneGeographique(9, "Espèces marines et d'eau douce des Antilles françaises", "Guadeloupe, Martinique et autres", 71731)
                     );
                     dbContext.zoneGeographiqueDao.createOrUpdate(
-                            new ZoneGeographique(10, "Espèces marines et d'eau douce de Guyanne française", "Guyanne", 2225263)
+                            new ZoneGeographique(10, "Espèces marines et d'eau douce de Guyanne française", "Guyanne", 2225263, dbContext.zoneGeographiqueDao.queryForId(9))
                     );
                     dbContext.zoneGeographiqueDao.createOrUpdate(
                             new ZoneGeographique(11, "Habitats subaquatiques", "Herbiers, Fonds, Littoraux,...", 1090239)
